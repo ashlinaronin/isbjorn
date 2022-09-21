ee069578a29dbaab0a99121948371db903b6f3bc
ashlinaronin <ashlinaronin@gmail.com>
2022-09-21 11:22:53 -0700

    Implement time manipulation- basically same as 2d from video but we need to track rotation too

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class TimeController : MonoBehaviour
 {
 
-    public static float gravity = -100;
+    // public static float gravity = -100;
 
     public struct RecordedData
     {
-        Vector3 position;
-        Vector3 velocity;
+        public Vector3 position;
+        public Quaternion rotation;
+        public Vector3 velocity;
     }
 
-    RecordedData[][] recordedData;
+    RecordedData[,] recordedData;
+    int recordMax = 100000;
+    int recordCount;
+    int recordIndex; 
+    bool wasReplaying = false;
 
     TimeControlled[] timeObjects;
 
@@ -21,15 +26,10 @@ public class TimeController : MonoBehaviour
     private void Awake()
     {
         timeObjects = GameObject.FindObjectsOfType<TimeControlled>();
+        recordedData = new RecordedData[timeObjects.Length, recordMax];
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         bool pause = Input.GetKey(KeyCode.H);
@@ -38,14 +38,66 @@ public class TimeController : MonoBehaviour
 
         if (stepBack)
         {
+            wasReplaying = true;
+
+
+            // note: tutorial did this opposite; i modified to use early return
+            if (recordIndex <= 0) return;
+
+            recordIndex--;
+
+            for (int objectIndex = 0; objectIndex < timeObjects.Length; objectIndex++)
+            {
+                TimeControlled timeObject = timeObjects[objectIndex];
+                RecordedData data = recordedData[objectIndex, recordIndex];
+                timeObject.transform.position = data.position;
+                timeObject.transform.rotation = data.rotation;
+                timeObject.velocity = data.velocity;
+            }
 
         }
         else if (pause && stepForward)
         {
+            wasReplaying = true;
+
+            // same as above - tutorial did opposite, i used early return
+            if (recordIndex >= recordCount - 1) return;
+
+            recordIndex++;
+
+            for (int objectIndex = 0; objectIndex < timeObjects.Length; objectIndex++)
+            {
+                TimeControlled timeObject = timeObjects[objectIndex];
+                RecordedData data = recordedData[objectIndex, recordIndex];
+                timeObject.transform.position = data.position;
+                timeObject.transform.rotation = data.rotation;
+                timeObject.velocity = data.velocity;
+            }
 
         }
+
+        // moving normally through time
         else if (!pause && !stepBack)
         {
+
+            if (wasReplaying)
+            {
+                recordCount = recordIndex;
+                wasReplaying = false;
+            }            
+
+            for (int objectIndex = 0; objectIndex < timeObjects.Length; objectIndex++)
+            {
+                TimeControlled timeObject = timeObjects[objectIndex];
+                RecordedData data = new RecordedData();
+                data.position = timeObject.transform.position;
+                data.rotation = timeObject.transform.rotation;
+                data.velocity = timeObject.velocity;
+                recordedData[objectIndex, recordCount] = data;
+            }
+            recordCount++;
+            recordIndex = recordCount;
+
             foreach (TimeControlled timeObject in timeObjects)
             {
                 timeObject.TimeUpdate();
